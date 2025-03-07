a5aa7b75a9bc3a83222e0a933b3a1b136b15615e
Theo Cholewa <theo.cholewa@etu.univ-cotedazur.fr>
2025-03-07 22:18:59 +0100

    #6 feat Read dialogs in json

--- Assets/Scripts/Planete3/DialogueManager.cs
+++ Assets/Scripts/Planete3/DialogueManager.cs
@@ -2,19 +2,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.IO; 
+using Newtonsoft.Json;
 
 public class DialogueManager : MonoBehaviour
 {
     public TextMeshProUGUI text; // TextMeshPro pour afficher le dialogue
     public Button nextButton; // Bouton "Suivant" ou "Fermer"
     public List<Dialogue> dialogues; // Liste des objets de type Dialogue
-
     private Dialogue currentDialogue; // Référence au Dialogue actif
 
     void Start()
     {
         text.gameObject.SetActive(false);
         nextButton.gameObject.SetActive(false);
+        
+        string filePath = Path.Combine(Application.dataPath, "Datas/Planete3/dialogs.json");
+        LoadAndAssignDialogues(filePath);
+    }
+
+    public void LoadAndAssignDialogues(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            // Lire le contenu du fichier JSON
+            string jsonData = File.ReadAllText(filePath);
+
+            // Désérialiser le JSON en un dictionnaire
+            Dictionary<string, List<string>> dialogueData = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(jsonData);
+
+            foreach (var entry in dialogueData)
+            {
+                Debug.Log($"Clé : {entry.Key}, Valeur : {entry.Value[0]}");
+                // Trouver l'objet dans la scène portant le nom correspondant
+                GameObject obj = GameObject.Find(entry.Key);
+                if (obj != null && obj.TryGetComponent<Dialogue>(out Dialogue dialogueComponent))
+                {
+                    // Affecter les données au composant Dialogue
+                    dialogueComponent.dialogues = entry.Value.ToArray();
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError($"Fichier JSON introuvable : {filePath}");
+        }
     }
 
     public void DisplayDialogue(Dialogue dialogue)
@@ -54,12 +86,10 @@ public class DialogueManager : MonoBehaviour
         UpdateDialogue();
     }
 
-
     private void CloseDialogue()
     {
         text.gameObject.SetActive(false);
         nextButton.gameObject.SetActive(false);
         currentDialogue = null; // Réinitialise le dialogue actif
     }
-
 }
